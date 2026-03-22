9839caa3f5605a1fa8c6bf5c272966709be16a20
qwuewo <lissnesrabu@gmail.com>
2026-03-22 22:15:00 +0300

    Update2 UnitGunner.java

--- UnitGunner.java
+++ UnitGunner.java
@@ -3,6 +3,12 @@ import java.awt.*;
 public class UnitGunner extends GameObject {
 
     @Override
+    protected void update(float dt) {
+        // движение влево
+        x -= 100 * dt; // скорость движения
+    }
+
+    @Override
     protected void draw(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
@@ -13,66 +19,55 @@ public class UnitGunner extends GameObject {
 
         // шлем
         g2.setColor(new Color(55, 120, 14));
-        g2.fillOval(offsetX + 180, offsetY + 40, 120, 120);
+        g2.fillOval(offsetX + 45, offsetY + 10, 30, 30);
         g2.setColor(Color.BLACK);
-        g2.drawOval(offsetX + 180, offsetY + 40, 120, 120);
-
-        // защита
-        g2.setColor(new Color(30, 73, 5));
-        g2.fillOval(offsetX + 265, offsetY + 75, 45, 70);
-        g2.setColor(Color.BLACK);
-        g2.drawOval(offsetX + 265, offsetY + 75, 45, 70);
+        g2.drawOval(offsetX + 45, offsetY + 10, 30, 30);
 
         // лицо
         g2.setColor(new Color(255, 233, 214));
-        g2.fillRoundRect(offsetX + 205, offsetY + 90, 65, 55, 15, 15);
+        g2.fillRoundRect(offsetX + 50, offsetY + 20, 16, 14, 4, 4);
         g2.setColor(Color.BLACK);
-        g2.drawRoundRect(offsetX + 205, offsetY + 90, 65, 55, 15, 15);
+        g2.drawRoundRect(offsetX + 50, offsetY + 20, 16, 14, 4, 4);
 
         // глаза
-        g2.fillOval(offsetX + 218, offsetY + 110, 8, 8);
-        g2.fillOval(offsetX + 245, offsetY + 110, 8, 8);
-
-        // рот
-        g2.setStroke(new BasicStroke(3));
-        g2.drawArc(offsetX + 225, offsetY + 125, 20, 10, 180, 180);
+        g2.fillOval(offsetX + 53, offsetY + 25, 2, 2);
+        g2.fillOval(offsetX + 60, offsetY + 25, 2, 2);
 
         // тело
-        g2.setStroke(new BasicStroke(4));
         g2.setColor(new Color(55, 120, 14));
-        g2.fillRoundRect(offsetX + 210, offsetY + 145, 85, 65, 20, 20);
+        g2.fillRoundRect(offsetX + 51, offsetY + 36, 22, 18, 5, 5);
         g2.setColor(Color.BLACK);
-        g2.drawRoundRect(offsetX + 210, offsetY + 145, 85, 65, 20, 20);
+        g2.drawRoundRect(offsetX + 51, offsetY + 36, 22, 18, 5, 5);
 
-        // АВТОМАТ
+        // автомат полностью
         // приклад
         g2.setColor(new Color(60, 60, 70));
-        g2.fillRoundRect(offsetX + 255, offsetY + 150, 65, 45, 8, 8);
+        g2.fillRoundRect(offsetX + 64, offsetY + 37, 16, 11, 2, 2);
         g2.setColor(Color.BLACK);
-        g2.drawRoundRect(offsetX + 255, offsetY + 150, 65, 45, 8, 8);
+        g2.drawRoundRect(offsetX + 64, offsetY + 37, 16, 11, 2, 2);
 
         // основа
         g2.setColor(new Color(85, 90, 100));
-        g2.fillRoundRect(offsetX + 100, offsetY + 155, 160, 30, 8, 8);
+        g2.fillRoundRect(offsetX + 25, offsetY + 39, 40, 7, 2, 2);
         g2.setColor(Color.BLACK);
-        g2.drawRoundRect(offsetX + 100, offsetY + 155, 160, 30, 8, 8);
+        g2.drawRoundRect(offsetX + 25, offsetY + 39, 40, 7, 2, 2);
 
         // ствол
         g2.setColor(new Color(70, 70, 80));
-        g2.fillRect(offsetX + 55, offsetY + 163, 45, 14);
+        g2.fillRect(offsetX + 14, offsetY + 41, 11, 3);
         g2.setColor(Color.BLACK);
-        g2.drawRect(offsetX + 55, offsetY + 163, 45, 14);
+        g2.drawRect(offsetX + 14, offsetY + 41, 11, 3);
 
         // магазин
         g2.setColor(new Color(75, 75, 85));
-        g2.fillRect(offsetX + 225, offsetY + 185, 22, 32);
+        g2.fillRect(offsetX + 56, offsetY + 46, 5, 8);
         g2.setColor(Color.BLACK);
-        g2.drawRect(offsetX + 225, offsetY + 185, 22, 32);
+        g2.drawRect(offsetX + 56, offsetY + 46, 5, 8);
 
         // рукоять
         g2.setColor(new Color(60, 60, 70));
-        g2.fillRoundRect(offsetX + 165, offsetY + 185, 18, 28, 6, 6);
+        g2.fillRoundRect(offsetX + 41, offsetY + 46, 5, 7, 2, 2);
         g2.setColor(Color.BLACK);
-        g2.drawRoundRect(offsetX + 165, offsetY + 185, 18, 28, 6, 6);
+        g2.drawRoundRect(offsetX + 41, offsetY + 46, 5, 7, 2, 2);
     }
 }
