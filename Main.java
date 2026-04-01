05ab2379afe0252c8d3edfae32baa3081f4529b5
FanTom <109598687+aFanTomka@users.noreply.github.com>
2026-04-02 02:11:28 +0300

    upp spawn tower
    Reduced the tower's size
    Corrected the system coordinates

--- Main.java
+++ Main.java
@@ -1,17 +1,22 @@
-package org.vlg;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
-    // Морозенко - GameObject
+
     public static void main(String[] args) {
+        Engine engine = Engine.getInstance();
+        Tower tower = new Tower(1, 3350, 900, 50, 0);
+        tower.setFraction(0);
+        engine.spawnObject(tower);
+        GameView gameView = new GameView(engine);
         JFrame frame = new JFrame("Untitled Game");
         frame.setSize(400, 400);
         frame.setLayout(new BorderLayout());
-        frame.add(new Control(), BorderLayout.SOUTH);
+        frame.add(gameView);
+        frame.add(new Controls(engine), BorderLayout.SOUTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
     }
 }
